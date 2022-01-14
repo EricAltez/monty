cab3a190894678e46398114001872d82691106ee
EricAltez <ericaltez1111@gmail.com>
2022-01-14 05:53:12 -0800

    add monty functions 2

--- montyfun2.c
+++ montyfun2.c
@@ -4,4 +4,16 @@
  *
  *
  */
-void m_swap(stack_t **stack, unsigned int line_number)
+void m_add(stack_t **stack, unsigned int line_number)
+{
+
+}
+/**
+ *
+ *
+ *
+ */
+void m_nop(stack_t **stack, unsigned int line_number)
+{
+
+}
